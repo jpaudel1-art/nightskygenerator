b89f3838d11cd6296dba0c6150714b25e1cfa047
bryannchoutpouen <bnchout1@students.towson.edu>
2026-05-12 10:57:46 -0400

    Made the background 3D

--- sketch.js
+++ sketch.js
@@ -1,22 +1,53 @@
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 }
+let stars = [];
+
+function setup(){
+  createCanvas(windowWidth, windowHeight, WEBGL);
+
+
+for(let i = 0; i < 500; i++){
+  stars.push({
+    x: random(-width, width),
+    y: random(-height, height),
+    z: random(-1000, 1000),
+    size: random(1, 4)
+  });
+}
+}
 
 function draw() {
   // Midnight Blue Background
   background(5, 10, 30); 
+
+  rotateY(frameCount * 0.0008);
+  rotateX(frameCount * 0.0003);
+
+  //Stars in 3D space
+  for(let stars of stars) {
+    push();
+    translate(star.x, star.y, star.z);
+    noStroke();
+    fill(255, random(150, 255));
+    sphere(star.size);
+    pop();
+
+    star.z += 2;
+
+    if(star.z > 1000){
+      star.z = -1000;
+      star.x = random(-width, width);
+      star.y = random(-height, height);
+    }
+  }
   
   // Draw a simple moon to test
   push();
-  translate(-300, -150, 0);
-
+  translate(250, -180, -300);
   noStroke();
-  ambientLight(150);
-  pointLight(255, 255, 255, 0, 0, 300);
-
-  fill(240, 240, 220);
+  fill(230, 230,200);
   sphere(60);
-
   pop();
 
   // Instructions text
