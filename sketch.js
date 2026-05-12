a12daf71f5b8879d525658ed3630d1f3bcf3e055
jenisha905 <158182466+jenisha905@users.noreply.github.com>
2026-05-12 11:26:52 -0400

    Added 3D stars to the background

--- sketch.js
+++ sketch.js
@@ -1,6 +1,20 @@
+let stars = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+
+  // stars around 800
+
+  for (let i = 0; i < 800; i++) {
+    stars.push({
+      x: random(-width * 2, width * 2),
+      y: random(-height * 2, height * 2),
+      z: random(-2000, 200), 
+      size: random(0.5, 4) 
+    });
+  }
 }
+
 
 function draw() {
   // Midnight Blue Background
@@ -18,6 +32,16 @@ function draw() {
   sphere(60);
 
   pop();
+
+  //stars
+  for (let s of stars) {
+    push();
+    translate(s.x, s.y, s.z);
+    fill(255); 
+    noStroke();
+    sphere(s.size); 
+    pop();
+  }
 
   // Instructions text
   fill(255);
